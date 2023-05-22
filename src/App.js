1a9007ccd0619264356c68ddab1fa23dfa6d028e
rosemutai <chepngetichrose2030@gmail.com>
2023-05-22 12:53:22 +0300

    Add Home page Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>React Capstone Project</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
