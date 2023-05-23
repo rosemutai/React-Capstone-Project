ce7280c6e1179e0fdd28e6ed2cfaca9286a17f7f
rosemutai <chepngetichrose2030@gmail.com>
2023-05-23 17:23:26 +0300

    Add search functionality

--- src/redux/statistics/statisticsSlice.js
+++ src/redux/statistics/statisticsSlice.js
@@ -3,27 +3,55 @@ import axios from 'axios';
 
 const initialState = {
   statistics: [],
+  countryDetails: [],
 };
 
-// const url = 'https://api.openbrewerydb.org/v1/breweries';
 const url = 'https://disease.sh/v3/covid-19/countries';
 export const fetchData = createAsyncThunk('statistics/fetchData', async () => {
   try {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    throw new Error(error);
+    throw new Error('Failed to fetch Data');
+  }
+});
+
+export const getCountryDetails = createAsyncThunk('statistics/getCountryDetails', async (country) => {
+  try {
+    const res = await axios.get(`https://disease.sh/v3/covid-19/countries/${country}`);
+    return res.data;
+  } catch (error) {
+    throw new Error('Failed to fetch Data');
+  }
+});
+
+export const getSearchedCountry = createAsyncThunk('statistics/getCountryDetails', async (country) => {
+  try {
+    const res = await axios.get(`https://disease.sh/v3/covid-19/countries/${country}`);
+    return res.data;
+  } catch (error) {
+    throw new Error('Failed to fetch Data');
   }
 });
 
 const StatisticsSlice = createSlice({
   name: 'statistics',
   initialState,
-  extraReducers: (builder) => {
-    builder.addCase(fetchData.fulfilled, (state, action) => {
+  extraReducers: {
+    [fetchData.fulfilled]: (state, action) => {
       state.statistics = action.payload;
-    });
+    },
+
+    [getCountryDetails.fulfilled]: (state, action) => (
+      { ...state, countryDetails: action.payload }
+    ),
+
+    [getSearchedCountry.fulfilled]: (state, action) => (
+      { ...state, countryDetails: action.payload }
+
+    ),
   },
 });
 
 export default StatisticsSlice.reducer;
+export const getSelectedCountry = (state) => state.statistics.countryDetails;
