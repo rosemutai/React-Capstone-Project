c2554b759d0e524324866f57e25bec9ccead15e4
rosemutai <chepngetichrose2030@gmail.com>
2023-05-23 11:07:46 +0300

    Add background color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchData } from './redux/statistics/statisticsSlice';
 import Home from './components/Home';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App w-full relative">
+      <div className="absolute z-30 bg-hotPink/60 pink600/40 w-full min-h-full">.</div>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
