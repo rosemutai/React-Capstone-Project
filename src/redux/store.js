5a1be4af40ebd6098f9aef73c5331e8a8f69a4b4
rosemutai <chepngetichrose2030@gmail.com>
2023-05-23 11:13:19 +0300

    Configure redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import StatisticsReducer from './statistics/statisticsSlice';
 
 export const store = configureStore({
-  reducer: {}
-})
+  reducer: {
+    statistics: StatisticsReducer,
+  },
+});
