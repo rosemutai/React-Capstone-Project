1f5522715577ee34e09c2d3c651160417a5a8a09
rosemutai <chepngetichrose2030@gmail.com>
2023-05-23 17:20:49 +0300

    Modify country's card

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
-const CountryCard = ({ country, active }) => {
-  console.log('Hey');
-  return (
+const CountryCard = ({ country, active }) => (
+  <Link to={country}>
     <div
-      className="card w-48 h-48 border border-slate-100 object-contain relative flex flex-col justify-between
-      items-end text-white pr-3"
+      className="card w-48 h-48 border border-grey-50 object-contain relative flex flex-col justify-between
+        items-end text-white pr-3"
     >
       <div className="span-icon absolute z-50 pt-5">
         <span className="material-symbols-outlined">arrow_circle_right</span>
@@ -17,11 +17,9 @@ const CountryCard = ({ country, active }) => {
         </h4>
         <p className="font-semibold text-sm">{active}</p>
       </div>
-
-      {/* <img className="w-8 h-8 object-contain" src={flag} alt="flag" /> */}
     </div>
-  );
-};
+  </Link>
+);
 
 CountryCard.defaultProps = {
   active: '',
